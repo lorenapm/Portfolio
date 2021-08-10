abbfff80f7cbccd65baa3f38f4f4411ac108067c
Lorena <lorena.perez@gmail.com>
2021-08-10 11:53:05 +0200

    add menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "../images/logo-prueba.png";
+import "../styles/header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__menu">
         <a href="">
-          <img src={logo} alt="" />
+          <img
+            className="header__menu-logo"
+            src={logo}
+            alt="logo Lorena Perez Martinez"
+          />
         </a>
         <div className="header__nav">
           <a href="">Portfolio</a>
