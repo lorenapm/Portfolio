5a326508db6700151106bb1920bc6eb4ec2eb679
Lorena <lorena.perez@gmail.com>
2021-08-10 14:14:11 +0200

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        <a href="https://github.com/lorenapm" target="_blank">
+          Visita mi portfolio en GitHub
+        </a>
+      </p>
+      <small>© Lorena Pérez Martínez 2021</small>
+    </div>
+  );
+}
+
+export default Footer;
